7d8dc4623adcb0a178f6d2e29490d225216911d4
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2019-07-24 20:28:35 +0200

    (#17) Use filename instead of full filepath in logging
    Close #17

--- src/log.h
+++ src/log.h
@@ -1,16 +1,20 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <string.h>
+
 // TODO(#16): Let's make loglevels
 
 #define LEVEL_ERROR 0
 #define LEVEL_WARN 1
 
-#define LOG(level, text) my_log(level, __FILE__, __LINE__, __FUNCTION__, text)
+#define FILENAME(filepath) (strrchr(filepath, '/') ? strrchr(filepath, '/') + 1 : filepath)
+
+#define LOG(level, text) my_log(level, FILENAME(__FILE__), __LINE__, __FUNCTION__, text)
 #define LOGE(text) LOG(LEVEL_ERROR, text)
 #define LOGW(text) LOG(LEVEL_WARN, text)
 
 // TODO(#17): Filepath in logging is too long. Use filename instead
-void my_log(int level, const char * filepath, int line, const char * function, const char * text);
+void my_log(int level, const char * filename, int line, const char * function, const char * text);
 
 #endif // LOG_H
