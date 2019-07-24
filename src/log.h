e6552bdb953019fb2cd1dd7160ae401588cdafcb
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2019-07-24 18:59:29 +0200

    (#15) Introduce log function
    Close #15

--- src/log.h
+++ src/log.h
@@ -3,6 +3,14 @@
 
 // TODO(#16): Let's make loglevels
 
-void my_log(const char * text);
+#define LEVEL_ERROR 0
+#define LEVEL_WARN 1
+
+#define LOG(level, text) my_log(level, __FILE__, __LINE__, __FUNCTION__, text)
+#define LOGE(text) LOG(LEVEL_ERROR, text)
+#define LOGW(text) LOG(LEVEL_WARN, text)
+
+// TODO: Filepath in logging is too long. Use filename instead
+void my_log(int level, const char * filepath, int line, const char * function, const char * text);
 
 #endif // LOG_H
--- test/log_test.h
+++ test/log_test.h
@@ -1,8 +1,9 @@
-#include "../src/tokenizer/token.h"
+#include "../src/log.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 int log_test_01() {
+    LOGE("Error");
     return 0;
 }
