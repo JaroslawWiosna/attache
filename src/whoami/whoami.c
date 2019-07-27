e77ad1b67c36d245dab6174759754bb6d62b690c
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2019-07-27 20:37:21 +0200

    (#38) Detect if config file exists

--- src/whoami/whoami.c
+++ src/whoami/whoami.c
@@ -1,9 +1,26 @@
+#include <sys/stat.h>
+#include <stdlib.h>
 #include "whoami.h"
 #include "../log.h"
 
 void whoami(int c, char* v[]) {
     LOGD("Entering function");
-    LOGE("Not implemented! :-(");
+    char* configFilepath = "attache.config";
+    struct stat* buf = malloc(sizeof(struct stat));
+
+    stat(configFilepath, buf);
+
+    // LOGI("size of file is %d", buf->st_size);
+    printf("size of file is %d\n", (int)buf->st_size);
+
+    if ((int)buf->st_size > 0) {
+        SLOGI("File %s exists", configFilepath);
+    } else {
+        SLOGI("File %s does not exist", configFilepath);
+    }
+
+    free(buf);
+
     (void)c;
     (void)v;
 }
